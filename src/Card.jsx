b04a84c55c6c470a504a78bf1f586e367f2327de
admin(kev) <admink@localhost.localdomain>
2024-02-16 14:57:11 +0300

    updated message

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,7 +10,7 @@ export default () => (
       </div>
     </div>
     <div className="cardInside">
-      <h3 className="back">Happy Birthday Dad!</h3>
+      {/* <h3 className="back">Happy Birthday Dad!</h3> */}
       <p>Dear Dad,</p>
       <p>
         On the occasion of your birthday, I want to tell you that you are an
