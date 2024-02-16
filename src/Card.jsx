70473a0e3d9727fea780ce4a748e6039d1bb5e8a
admin(kev) <admink@localhost.localdomain>
2024-02-16 14:42:04 +0300

    ready

---
+++ src/Card.jsx
@@ -0,0 +1,23 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">Happy Birthday Dad!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">Happy Birthday Dad!</h3>
+      <p>Dear Dad,</p>
+      <p>
+        Another day to celebrate the man you are! I want you to know that you
+        are the most special person in my life. You are loved and appreciated.
+        Happy Birthday, Dad!
+      </p>
+      <p className="name">Kev</p>
+    </div>
+  </div>
+);
